b4be5392924a1f67d0fbc23b0671826ce3aa1a84
Andre Pereira <adrapereira@gmail.com>
2018-07-22 14:31:12 +0100

    More debugging/testing

--- routes/signup.js
+++ routes/signup.js
@@ -7,14 +7,14 @@ const Team = require('../models/Team');
 
 router.get('/', function(req, res) {
     res.status(200).end(); // best practice to respond with 200 status
-    console.log("req: " + JSON.stringify(req));
+    console.log("code: " + JSON.stringify(req.query.code));
     console.log("env: " + JSON.stringify(process.env));
     console.log("client_id: " + process.env.CLIENT_ID);
     console.log("client_secret: " + process.env.CLIENT_SECRET);
     slack.oauth.token({
         client_id: process.env.CLIENT_ID,
         client_secret: process.env.CLIENT_SECRET,
-        code: req.params.code
+        code: req.query.code
     }).then(function(data){
         console.log(data);
         const team = new Team(data.team_id, data.team_name, data.access_token);
