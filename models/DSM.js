c03ba24822cecef6a78df064424159e50fd9ca23
Andre Pereira <adrapereira@gmail.com>
2018-07-28 18:58:50 +0100

    Set the default duration as 15 minutes

--- models/DSM.js
+++ models/DSM.js
@@ -7,8 +7,22 @@ class DSM {
         this.message_ts;
         this.manage_message_ts;
         this.participants = {};
-        this.duration;
+        this._duration;
         this.link;
+    }
+
+    get duration() {
+        if (this._duration) {
+            return this._duration;
+        }
+        if (this.team.settings.dsmDuration) {
+            return this.team.settings.dsmDuration;
+        }
+        return "15";
+    }
+
+    set duration(dur) {
+        this._duration = dur;
     }
 
     mapObjectToThis(obj) {
