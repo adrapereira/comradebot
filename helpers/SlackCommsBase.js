1c24e6670c51561a03125309558a6d6872d39b56
Andre Pereira <adrapereira@gmail.com>
2018-08-01 23:38:05 +0100

    Changed from conversation to channel to get members

--- helpers/SlackCommsBase.js
+++ helpers/SlackCommsBase.js
@@ -36,7 +36,7 @@ module.exports = {
         });
     },
     getChannelUsers: function (token, channel) {
-        return slack.conversations.members({
+        return slack.channels.info({
             token: token,
             channel: channel
         });
