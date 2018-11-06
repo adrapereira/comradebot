70d33baebbfcfd7ba08518cd5e6aac7459e7d464
Andre Pereira <adrapereira@gmail.com>
2018-11-06 09:03:12 +0000

    Deleted charts service

--- routes/test.js
+++ routes/test.js
@@ -2,10 +2,7 @@ const express = require('express');
 const router = express.Router();
 require('dotenv').config();
 
-const ChartsService = require('../helpers/ChartsService');
-
 router.get('/', function(req, res) {
-    ChartsService.generateDoughnut("bla");
 });
 
 module.exports = router;
